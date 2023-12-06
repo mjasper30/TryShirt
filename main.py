3ae99b516ccf3d8111d0ac4d8d290e82f659baf2
mjasper30 <jasper.macaraeg42@gmail.com>
2023-12-06 14:34:14 +0800

    shirt size done - by 6ft

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 import cvzone
 from cvzone.PoseModule import PoseDetector
 
+#cap = cv2.VideoCapture("Resources/Videos/output.mp4")
 cap = cv2.VideoCapture(0)
 detector = PoseDetector()
 shirtsFolderPath = "backend/uploads"
@@ -16,6 +17,7 @@ imgButtonLeft = cv2.flip(imgButtonRight, 1)
 counterRight = 0
 counterLeft = 0
 selectionSpeed = 20
+shirt_size = ""
 
 # Create a named window and set it to fullscreen
 cv2.namedWindow("Image", cv2.WINDOW_NORMAL)
@@ -23,7 +25,7 @@ cv2.setWindowProperty("Image", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 while True:
     success, img = cap.read()
-    img = detector.findPose(img, draw=False)
+    img = detector.findPose(img, draw=True)
     lmList, bboxInfo = detector.findPosition(img, bboxWithHands=False, draw=False)
     
     if lmList:
@@ -33,7 +35,16 @@ while True:
         lm24 = lmList[24][0:2]  # Assuming lmList[24] corresponds to the left shoulder
 
         # Calculate the distance between the shoulders to estimate the width of the person
-        person_width = math.dist(lm23, lm24)
+        person_width = int(math.dist(lm11, lm12))
+        person_length = int(math.dist(lm11, lm23))
+        person_width_inch = 0;
+        person_length_inch = 0;
+        # person_width_inch = int(math.dist(lm11, lm12)) - 105
+        # person_length_inch = int(math.dist(lm11, lm23)) - 161
+
+        print(int(math.dist(lm11, lm12)))
+        print(int(math.dist(lm11, lm23)))
+        # print(person_width, person_length)
 
         imgShirt = cv2.imread(os.path.join(shirtsFolderPath, listShirts[imageNumber]), cv2.IMREAD_UNCHANGED)
 
@@ -53,15 +64,30 @@ while True:
         img = cvzone.overlayPNG(img, imgButtonRight, (530, 200))
 
         # Determine the shirt size based on the estimated person width
-        if person_width < 100:
+        if 123 > person_width or 184 > person_length:
+            shirt_size = "Extra Small"
+        elif 128 > person_width or 187 > person_length:
             shirt_size = "Small"
-        elif 100 <= person_width < 150:
+        elif 133 > person_width or 190 > person_length:
             shirt_size = "Medium"
-        else:
+        elif 138 > person_width or 193 > person_length:
             shirt_size = "Large"
+        elif 143 > person_width or 196 > person_length:
+            shirt_size = "XL"
+        elif 148 > person_width or 197 > person_length:
+            shirt_size = "XXL"
+        else:
+            shirt_size = "Getting Shirt Size..."
+            person_width = ""
+            person_length = ""
+            pass
 
         # Display the shirt size on the window
-        cv2.putText(img, f"Shirt Size: {shirt_size}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.putText(img, f"Shirt Size: {shirt_size}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        cv2.putText(img, f"Shirt Width: {person_width_inch} inch", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+        cv2.putText(img, f"Shirt Length: {person_length_inch} inch", (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         print("Shirt Size:", shirt_size)
 
